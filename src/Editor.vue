ce0bcad8722c98f1987221cd022d1ddea82557eb
Filip Rak <filip.rak@ikol.com>
2026-07-18 17:37:56 +0200

    Work in progress

--- src/Editor.vue
+++ src/Editor.vue
@@ -15,7 +15,7 @@ import ace from "ace-builds/src-noconflict/ace";
 
 ace.config.set(
     "basePath",
-    "/node_modules/ace-builds/src-noconflict"
+    "https://cdn.jsdelivr.net/npm/ace-builds@1.44.0/src-min-noconflict/"
 );
 
 // Theme
